b66d182d7cb99824016db156d4d22d862dffb193
mafesilvapires <nanda.dasilvapires@gmail.com>
2026-08-30 23:02:00 -0300

    finaliza estilização das telas de autenticação e home

--- authenticate_user/views.py
+++ authenticate_user/views.py
@@ -1,30 +1,30 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
 
-# Create your views here.
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
 
     username = request.POST.get('username')
-    email =  request.POST.get('email')
-    senha =  request.POST.get('senha')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
 
     if User.objects.filter(username=username).exists():
-        return HttpResponse("Usuário já cadastrado")
+        messages.error(request, "Usuário já cadastrado")
+        return render(request, 'cadastro.html')
 
     user = User(
-            username=username,
-            email=email,
-        )
-
+        username=username,
+        email=email,
+    )
     user.set_password(senha)
     user.save()
 
-    return HttpResponse("Usuário cadastrado com sucesso")
+    messages.success(request, "Usuário cadastrado com sucesso! Faça login.")
+    return redirect("login")
 
 def login(request):
     if request.method == "GET":
@@ -34,16 +34,17 @@ def login(request):
     senha = request.POST.get("senha")
 
     user = authenticate(
-            request,
-            username=username,
-            password=senha,
-        )
+        request,
+        username=username,
+        password=senha,
+    )
 
     if user is not None:
         auth_login(request, user)
         return redirect("plataforma")
 
-    return HttpResponse("Usuário ou senha inválidos")
+    messages.error(request, "Usuário ou senha inválidos")
+    return render(request, 'login.html')
 
 @login_required
 def plataforma(request):
