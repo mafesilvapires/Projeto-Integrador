aed5bdd4cf49109d0006c2f6bb3f0ce99f015b99
thebas <thebas2501@gmail.com>
2026-09-06 00:26:10 -0300

    feat: Configura servidor smtp e recuperacao de senha

--- authenticate_user/urls.py
+++ authenticate_user/urls.py
@@ -1,11 +1,40 @@
-from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.urls import path, include, reverse_lazy
 from . import views
 
 urlpatterns = [
+    # Paths de autenticação e MFA
     path('cadastro/', views.cadastro, name='cadastro'),
     path('qrcode/<str:username>/', views.qrcode_2fa, name='qrcode'),
     path('login/', views.login, name='login'),
     path('verificar-2fa/', views.verificar_2fa, name='verificar_2fa'),
     path('plataforma', views.plataforma, name='plataforma'),
     path('', include('django.contrib.auth.urls')),
+
+    # Paths de recuperação de senha
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name = 'registration/password_reset_form.html',
+        email_template_name = 'registration/password_reset_email.txt',
+        subject_template_name = 'registration/password_reset_subject.txt',
+        success_url=reverse_lazy('password_reset_done')
+        ), name = 'password_reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name = 'registration/password_reset_done.html'
+        ), name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name = 'registration/password_reset_confirm.html',
+        success_url=reverse_lazy('password_reset_complete')
+        ), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'
+        ), name = 'password_reset_complete'),
 ]
+
+
+
+
+
+
--- config_app/settings.py
+++ config_app/settings.py
@@ -137,14 +137,15 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-# Email
+# Email smtp configuration
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
-
-EMAIL_BACKEND = {
-    'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
-    },
-}
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_HOST_PORT'))
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Parâmetros de Axes (rate_limit)
 AXES_FAILURE_LIMIT = 5
@@ -159,3 +160,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = "plataforma"
 LOGOUT_REDIRECT_URL = "login"
+
+#Parâmetros de site e token de redefinição
+SITE_ID = 1
+PASSWORD_RESET_TIMEOUT = 3600
